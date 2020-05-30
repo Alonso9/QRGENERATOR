1050afa8bf73bfa0d414d6851ccb25f48235708f
Alonso <ramonalonso_giron@ucol.mx>
2020-05-29 19:44:52 -0500

    This program make QR code and is programmed on python

---
+++ qrGenerator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''
+Python program to create qr codes, using 
+'''
+
+import qrcode
+from PIL import Image
+
+
+#Creating QR code:
+text = input("Introduce the text to make QR code: ")
+image = qrcode.make(text)
+
+#Generating the image:
+imageName = input("Introduce the image name without extension: ") + '.png'
+
+imageFile = open(imageName, 'wb')
+
+#Save the QR image
+image.save(imageFile)
+
+#Close the QR image
+imageFile.close()
+
+#Open image
+sourceimage = './' + imageName
+Image.open(sourceimage).show()
+
